a3e16b000d0bd6bdb21890df8da748083172008e
Taras Hupalo <taras.hupalo@gmail.com>
2016-04-26 21:38:04 +0300

    Clean up

--- anim.js
+++ anim.js
@@ -1,44 +1,26 @@
-function calculateMinutes(time) {
-  var split = time.split(':');
 
-  var hours = Number(split[0]);
-  var minutes = Number(split[1]);
-  return hours * 60 + minutes;
-}
-
-function calculateAngle(time) {
-  const maxMinutes = 24 * 60;
-  return 360 * calculateMinutes(time) / maxMinutes;
-}
-
-function calculateDuration(fromAngle, toAngle) {
-  const maxDuration = 5; // 5 seconds
-  var delta = Math.abs(fromAngle - toAngle);
-  return maxDuration * delta / 360;
-}
 
 function animate(time) {
-  var prevAngle = calculateAngle(prevTime);
-  var angle = calculateAngle(time);
-  var duration = calculateDuration(prevAngle, angle);
-
-  console.log(`time ${time}; angle ${angle}; duration ${duration}`);
-
-  var sun = document.querySelector('.sun-wrapper');
-  sun.style.transition = `transform ${duration}s linear`;
-
-  setSunPosition(angle);
-  // sun.style.transform = `rotate(${angle}deg)`;
+  Sunrise.update({
+    sunriseTime: '6:00',
+    sunsetTime: '15:40',
+    currentTime: time,
+    sunElement: document.querySelector('.sun-wrapper')
+  });
 }
 
+Sunrise.update({
+  sunriseTime: '6:00',
+  sunsetTime: '15:40',
+  currentTime: '6:30',
+  sunElement: document.querySelector('.sun-wrapper')
+});
 
 
-var prevTime = '00:00';
 
 document.querySelector('#start').addEventListener('click', function() {
   var time = document.querySelector('#current-time').value;
   animate(time);
-  prevTime = time;
 });
 
 var radioList = document.querySelectorAll('input[type="radio"');
@@ -47,6 +29,5 @@ radioArray.forEach(function(radio) {
   radio.addEventListener('click', function() {
     var time = radio.value;
     animate(time);
-    prevTime = time;
   });
 });
--- index.js
+++ index.js
@@ -1,63 +1,120 @@
-function animationData() {
-  function radToDeg(rad) {
-    return rad * 180 / Math.PI;
-  }
-  
-  const h = window.innerHeight / 2;
-  const L = window.innerWidth;
-  const sunRadius = document.querySelector('.sun-wrapper').offsetWidth / 2;
+const Sunrise = (function() {
+  function calculateMinutes(time) {
+    var split = time.split(':');
 
-  // var R = (0.5 * h) + (Math.pow(L, 2) / (8 * h));
-  const R = h;  // (2)
-  const alpha = 2 * Math.acos((R - h) / R);  // (3)
-  const gamma = 2 * Math.asin(sunRadius / (2 * R));  // (4)
-  const phi = alpha + 2 * gamma;  // (5)
+    var hours = Number(split[0]);
+    var minutes = Number(split[1]);
+    return hours * 60 + minutes;
+  }
+
+  function calculateAngle(time) {
+    const maxMinutes = 24 * 60;
+    return 360 * calculateMinutes(time) / maxMinutes;
+  }
+
+  function calculateDuration(fromAngle, toAngle) {
+    const maxDuration = 5; // 5 seconds
+    var delta = Math.abs(fromAngle - toAngle);
+    return maxDuration * delta / 360;
+  }
+
+
+  function animationData() {
+    function radToDeg(rad) {
+      return rad * 180 / Math.PI;
+    }
+
+    const h = window.innerHeight / 2;
+    const L = window.innerWidth;
+    const sunRadius = config.sunElement.offsetWidth / 2;
+
+    var R = (0.5 * h) + (Math.pow(L, 2) / (8 * h));  // (2)
+    //const R = h;
+    const alpha = 2 * Math.acos((R - h) / R);  // (3)
+    const gamma = 2 * Math.asin(sunRadius / (2 * R));  // (4)
+    const phi = alpha + 2 * gamma;  // (5)
+
+    return {
+      h: h,
+      L: L,
+      R: R,
+      alpha: radToDeg(alpha),
+      gamma: radToDeg(gamma),
+      phi: radToDeg(phi),
+      r: sunRadius
+    }
+  }
+
+  function prepareDomElements() {
+    var data = animationData();
+
+    var trajectory = document.querySelector('.trajectory');
+    trajectory.style.width = (data.R * 2) + 'px';
+    trajectory.style.height = (data.R * 2) + 'px';
+    trajectory.style.left = (data.L / 2 - data.R) + 'px';
+    trajectory.style.top = data.h + 'px';
+    //trajectory.style.top = 0;
+
+    var sun = document.querySelector('.sun-wrapper');
+    sun.style.top = data.h + 'px';
+    //sun.style.top = 0;
+    sun.style.transformOrigin = `50% ${data.R + data.r}px`;
+
+    console.log('alpha', data.alpha);
+    console.log('gamma', data.gamma);
+    console.log('phi', data.phi);
+  }
+
+  function moveSun() {
+    var data = animationData();
+
+    var angle = getCurrentAngle() - (data.phi / 2);
+    config.sunElement.style.transform = `rotate(${angle}deg)`;
+    console.log('angle', angle);
+  }
+
+  function updateConfig(_config) {
+    config.sunriseTime = _config.sunriseTime;
+    config.sunsetTime = _config.sunsetTime;
+    config.currentTime = _config.currentTime;
+    config.sunElement = _config.sunElement;
+  }
+
+  function getCurrentAngle() {
+    const data = animationData();
+
+    const sunriseInMinutes = calculateMinutes(config.sunriseTime);
+    const deltaMin = calculateMinutes(config.sunsetTime) - sunriseInMinutes;
+    const currentMin = calculateMinutes(config.currentTime) - sunriseInMinutes;
+
+    return data.phi * currentMin / deltaMin;
+  }
+
+  function updateAnimationDuration() {
+    if (config.prevTime) {
+      var prevAngle = calculateAngle(config.prevTime);
+      var angle = calculateAngle(config.currentTime);
+      var duration = calculateDuration(prevAngle, angle);
+      var sun = document.querySelector('.sun-wrapper');
+      sun.style.transition = `transform ${duration}s linear`;
+    }
+    config.prevTime = config.currentTime;
+  }
+
+
+  const config = {};
 
   return {
-    L: L,
-    R: R,
-    alpha: radToDeg(alpha),
-    gamma: radToDeg(gamma),
-    phi: radToDeg(phi),
-    r: sunRadius
+    update: function(_config) {
+      updateConfig(_config);
+
+      if (!config.rendered) {
+        prepareDomElements();
+        config.rendered = true;
+      }
+
+      moveSun();
+      updateAnimationDuration();
+    }
   }
-}
-
-function prepareSunrise(data) {
-  var data = animationData();
-
-  var sunContainer = document.querySelector('.trajectory');
-  sunContainer.style.width = (data.R * 2) + 'px';
-  sunContainer.style.height = (data.R * 2) + 'px';
-  sunContainer.style.left = (data.L / 2 - data.R) + 'px';
-// sunContainer.style.top = height / 2 + 'px';
-  sunContainer.style.top = 0;
-
-  var sun = document.querySelector('.sun-wrapper');
-// outer.style.top = height / 2 + 'px';
-  sun.style.top = 0;
-  sun.style.transformOrigin = `50% ${data.R + data.r}px`;
-
-  console.log('alpha', data.alpha);
-  console.log('gamma', data.gamma);
-  console.log('phi', data.phi);
-}
-
-function setSunPosition(offset) {
-  var data = animationData();
-
-  var sun = document.querySelector('.sun-wrapper');
-  var angle = offset - (data.phi / 2);
-  // var angle = offset - (data.alpha / 2);
-  sun.style.transform = `rotate(${angle}deg)`;
-}
-
-
-prepareSunrise();
-setSunPosition(0);
-
-
-
-
-
-
+})();
